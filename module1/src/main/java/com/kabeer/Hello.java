567d7ff4a892e561664230c5a85ca14c60b6e8ce
Kabeer Ahmed <smdahmed@gmail.com>
2014-11-11 16:38:32 +0000

    Full repo

---
+++ module1/src/main/java/com/kabeer/Hello.java
@@ -0,0 +1,12 @@
+package com.kabeer;
+
+public class Hello {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello Module 1");
+
+	}
+
+
+}
---
+++ module2/src/main/java/com/kabeer/Hai.java
@@ -0,0 +1,12 @@
+package com.kabeer;
+
+public class Hai {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hai Module 2");
+
+	}
+
+
+}
